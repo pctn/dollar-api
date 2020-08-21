03e7fa0b4e0002ebcf4af07c91590776694eed4e
Plinio Caetano <pliniocaetano@Plinios-MacBook-Pro.local>
2020-08-21 10:22:39 -0300

    Added Gradle build project

---
+++ src/main/java/com/dollar/dollarAPI/DollarApiApplication.java
@@ -0,0 +1,13 @@
+package com.dollar.dollarAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dollar/dollarAPI/DollarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dollar.dollarAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
